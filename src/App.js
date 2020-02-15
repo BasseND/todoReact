20d1f9377ad7e9120cb4aa0e3874c61b949a0c79
Bassirou NDIAYE <ndiayebasse@gmail.com>
2020-02-15 19:41:01 +0100

    Add components state and so

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Todos from './components/Todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Apprendre React',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Apprendre Drupal',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Apprendre Vue Js',
+        completed: false
+      },
+    ]
+  }
 
-export default App;
+  markComplete = (id) => {
+   // console.log(id)
+   this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo;
+   })});
+  }
+  
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos myprops={this.state.todos} markComplete={this.markComplete}/>
+      </div>
+     
+    )
+  }
+}
